8b39e92c14041d9c72700fddd536359e2e722426
ndurbin <nick.durbin@gmail.com>
2019-10-02 20:14:57 -0400

    Made some question.

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const dataSet = [
+  {
+    question: "Which colors do you prefer?",
+    answers: [
+      "Scarlet and gold",
+      "Yellow and black",
+      "Blue and bronze",
+      "Green and silver"
+    ],
+    correct: 1
+  },
+  {
+    question: "Which of these appeals to you?",
+    answers: [
+      "Braveryr",
+      "Loyalty",
+      "Knowledge",
+      "Cunning"
+    ],
+    correct: 3
+  },
+   {
+    question: "If you were an animal, which would you be?",
+    answers: [
+      "Lion",
+      "Badger",
+      "Eagle",
+      "Snake"
+    ],
+    correct: 3
+  },
+  {
+    question: "Your favorite ghost is ____?",
+    answers: [
+      "Nearly Headless Nick",
+      "Fat Friar",
+      "Grey Lady",
+      "Bloody Baron"
+    ],
+    correct: 2
+  },
+  {
+    question: "Which element would you be?",
+    answers: [
+      "Fire",
+      "Earth",
+      "Air",
+      "Water"
+    ],
+    correct: 0
+  },
+  {
+    question: "How would you prefer to enter your house?",
+    answers: [
+      "Give a password to a Fat Lady?",
+      "Tapping a rhythmic sound on a barrel?",
+      "Answering a riddle of logic?",
+      "Passing through dungeons?"
+    ],
+    correct: 3
+  },
+];
